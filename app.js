320f9da515d4f5e17fbe8fff2d23572eb8c53a82
maksym <paramudmaksym72@gmail.com>
2024-12-13 21:19:33 +0200

    final commit

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ function AboutCat (poroda, age, color, name, imgURL, isSleepping = false) {
         }
         console.log('hello');
     }
+
+    this.wakeUp = function() {
+        this.isSleepping = false;
+    }
+
+    this.takeANap = function() {
+        this.isSleepping = true;
+    }
 }
 
 let cat = [];
@@ -23,6 +31,7 @@ cat[2] = new AboutCat('Exotic Shorthair', 4, 'black', "Фелікс", "images/10
 
 let container = document.querySelector(".container");
 let catDiv = document.querySelector(".currentCat");
+let btn = document.querySelector(".btn");
 let btn1 = document.querySelector("#btn-1");
 let btn2 = document.querySelector("#btn-2");
 let btn3 = document.querySelector("#btn-3");
@@ -30,7 +39,26 @@ let btn3 = document.querySelector("#btn-3");
 btn1.addEventListener("click", function(e) {
     catDiv.innerHTML = `<img src = "${cat[0].imgURL}">
                         <li>порода: ${cat[0].poroda}</li>
-                        <li>age: ${cat[0].poroda}</li>
-                        <li></li>
-                        <li></li>`;
+                        <li>age: ${cat[0].age}</li>
+                        <li>color: ${cat[0].color}</li>
+                        <li>name: ${cat[0].name}</li>
+                        <li>status: ${cat[0].isSleepping}</li>`;
+});
+
+btn2.addEventListener("click", function(e) {
+    catDiv.innerHTML = `<img src = "${cat[1].imgURL}">
+                        <li>порода: ${cat[1].poroda}</li>
+                        <li>age: ${cat[1].age}</li>
+                        <li>color: ${cat[1].color}</li>
+                        <li>name: ${cat[1].name}</li>
+                        <li>status: ${cat[1].isSleepping}</li>`;
+});
+
+btn3.addEventListener("click", function(e) {
+    catDiv.innerHTML = `<img src = "${cat[2].imgURL}">
+                        <li>порода: ${cat[2].poroda}</li>
+                        <li>age: ${cat[2].age}</li>
+                        <li>color: ${cat[2].color}</li>
+                        <li>name: ${cat[2].name}</li>
+                        <li>status: ${cat[2].isSleepping}</li>`;
 });
